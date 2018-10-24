b3944afc3fbc559a22e6cb406e50e718fcb88c4e
SnowWalkerJ <jike3212001@163.com>
2018-10-24 11:12:10 +0800

    colors

--- drivers/screen.h
+++ drivers/screen.h
@@ -2,19 +2,30 @@
 #define MAX_ROWS 24
 #define MAX_COLS 80
 
-#define MAKE_STYLE(fore_color, back_color) (((back_color)<<4) | (fore_color))
-#define WHITE 0xf
-#define BLACK 0x0
-#define BLUE 0x1
-#define GREEN 0x2
-#define GREEN2 0x3
-#define RED   0x4
-#define PURPLE 0x5
-#define YELLOW 0x6
-#define GRAY 0x7
-#define WHITE_ON_BLACK MAKE_STYLE(WHITE, BLACK)
-#define GREEN_ON_BLACK   MAKE_STYLE(GREEN, BLACK)
-#define RED_ON_BLACK   MAKE_STYLE(RED, BLACK)
+#define VGA_MAKE_STYLE(fore_color, back_color) (((back_color)<<4) | (fore_color))
+#define VGA_BLACK 0x0
+#define VGA_BLUE 0x1
+#define VGA_GREEN 0x2
+#define VGA_CYAN 0x3
+#define VGA_RED   0x4
+#define VGA_MAGENTA 0x5
+#define VGA_BROWN 0x6
+#define VGA_LIGHTGRAY 0x7
+
+#define VGA_LIGHT(color) ((color)+8)
+
+#define VGA_DARKGRAY VGA_LIGHT(VGA_BLACK)
+#define VGA_LIGHTBLUE VGA_LIGHT(VGA_BLUE)
+#define VGA_LIGHTGREEN VGA_LIGHT(VGA_GREEN)
+#define VGA_LIGHTCYAN VGA_LIGHT(VGA_CYAN)
+#define VGA_LIGHTRED VGA_LIGHT(VGA_RED)
+#define VGA_LIGHTMAGENTA VGA_LIGHT(VGA_MAGENTA)
+#define VGA_LIGHTBROWN VGA_LIGHT(VGA_BROWN)
+#define VGA_WHITE VGA_LIGHT(VGA_LIGHTGRAY)
+
+#define WHITE_ON_BLACK VGA_MAKE_STYLE(VGA_WHITE, VGA_BLACK)
+#define GREEN_ON_BLACK VGA_MAKE_STYLE(VGA_GREEN, VGA_BLACK)
+#define RED_ON_BLACK   VGA_MAKE_STYLE(VGA_RED, VGA_BLACK)
 
 #define REG_SCREEN_CTRL 0x3d4
 #define REG_SCREEN_DATA 0x3d5
